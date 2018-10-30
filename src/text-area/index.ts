aaa7df6258937d935f0c2961dfa84680e2761dc9
Tom Dye <github@tomdye.co.uk>
2018-10-30 10:08:50 +0000

    re-arrange for material

--- src/text-area/index.ts
+++ src/text-area/index.ts
@@ -101,58 +101,14 @@ export class TextareaBase<P extends TextareaProperties = TextareaProperties> ext
 		event.stopPropagation();
 		this.properties.onKeyUp && this.properties.onKeyUp(event.which, () => { event.preventDefault(); });
 	}
-	private _onMouseDown (event: MouseEvent) {
-		event.stopPropagation();
-		this.properties.onMouseDown && this.properties.onMouseDown();
-	}
-	private _onMouseUp (event: MouseEvent) {
-		event.stopPropagation();
-		this.properties.onMouseUp && this.properties.onMouseUp();
-	}
-	private _onTouchStart (event: TouchEvent) {
-		event.stopPropagation();
-		this.properties.onTouchStart && this.properties.onTouchStart();
-	}
-	private _onTouchEnd (event: TouchEvent) {
-		event.stopPropagation();
-		this.properties.onTouchEnd && this.properties.onTouchEnd();
-	}
-	private _onTouchCancel (event: TouchEvent) {
-		event.stopPropagation();
-		this.properties.onTouchCancel && this.properties.onTouchCancel();
-	}
 
-	private _uuid: string;
-
-	constructor() {
-		super();
-		this._uuid = uuid();
-	}
-
-	protected getRootClasses(): (string | null)[] {
-		const {
-			disabled,
-			invalid,
-			readOnly,
-			required
-		} = this.properties;
-		const focus = this.meta(Focus).get('root');
-		return [
-			css.root,
-			disabled ? css.disabled : null,
-			focus.containsFocus ? css.focused : null,
-			invalid === true ? css.invalid : null,
-			invalid === false ? css.valid : null,
-			readOnly ? css.readonly : null,
-			required ? css.required : null
-		];
-	}
+	private _uuid = uuid();
 
 	render(): DNode {
 		const {
 			aria = {},
 			columns,
-			disabled,
+			disabled = false,
 			widgetId = this._uuid,
 			invalid,
 			label,
@@ -160,8 +116,8 @@ export class TextareaBase<P extends TextareaProperties = TextareaProperties> ext
 			minLength,
 			name,
 			placeholder,
-			readOnly,
-			required,
+			readOnly = false,
+			required = false,
 			rows,
 			value,
 			wrapText,
@@ -169,13 +125,30 @@ export class TextareaBase<P extends TextareaProperties = TextareaProperties> ext
 			labelHidden,
 			labelAfter
 		} = this.properties;
-		const focus = this.meta(Focus).get('root');
 
-		const children = [
+		const { containsFocus } = this.meta(Focus).get('root');
+
+		const classes = [
+			css.root,
+			disabled ? css.disabled : null,
+			containsFocus ? css.focused : null,
+			invalid === true ? css.invalid : null,
+			invalid === false ? css.valid : null,
+			readOnly ? css.readonly : null,
+			required ? css.required : null
+		];
+
+		return v('div', {
+			key: 'root',
+			classes: this.theme(classes)
+		}, [
 			label ? w(Label, {
+				extraClasses: {
+					root: this.theme(css.label)!
+				},
 				theme,
 				disabled,
-				focused: focus.containsFocus,
+				focused: containsFocus,
 				invalid,
 				readOnly,
 				required,
@@ -209,20 +182,10 @@ export class TextareaBase<P extends TextareaProperties = TextareaProperties> ext
 					oninput: this._onInput,
 					onkeydown: this._onKeyDown,
 					onkeypress: this._onKeyPress,
-					onkeyup: this._onKeyUp,
-					onmousedown: this._onMouseDown,
-					onmouseup: this._onMouseUp,
-					ontouchstart: this._onTouchStart,
-					ontouchend: this._onTouchEnd,
-					ontouchcancel: this._onTouchCancel
+					onkeyup: this._onKeyUp
 				})
 			])
-		];
-
-		return v('div', {
-			key: 'root',
-			classes: this.theme(this.getRootClasses())
-		}, labelAfter ? children.reverse() : children);
+		]);
 	}
 }
 
